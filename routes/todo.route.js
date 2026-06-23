6f881b3b1f7000cbaccd9c10b581e3eaa9c0f8ef
DefNotArham <arhamkabir231@gmail.com>
2026-06-23 08:14:45 -0400

    added all the controllers to routes

--- routes/todo.route.js
+++ routes/todo.route.js
@@ -1,7 +1,17 @@
 import express from "express";
 
+import { getAllTodosController } from "../controller/todo.controller.js";
+import { createNewTodoController } from "../controller/todo.controller.js";
+import { updateTodoController } from "../controller/todo.controller.js";
+import { deleteTodoController } from "../controller/todo.controller.js";
+import { getCompletedTodosController } from "../controller/todo.controller.js";
+
 const router = express.Router();
 
-router.get("/");
+router.get("/", getAllTodosController);
+router.post("/", createNewTodoController);
+router.patch("/:id", updateTodoController);
+router.delete("/:id", deleteTodoController);
+router.get("/completed", getCompletedTodosController);
 
 export default router;
